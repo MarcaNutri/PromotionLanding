f7238416e07b1cc9d41c247c32f61bfee6e46b7e
= <=>
2023-10-14 11:33:22 -0300

    update icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ const Document = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Jomhuria&family=Roboto:wght@400;500&display=swap" rel="stylesheet"/>
+        <link rel="icon" type="image/png" href="../../assets/logo-small.png"/>
       </Head>
       <body>
         <Main />
