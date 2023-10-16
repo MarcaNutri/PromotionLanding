4b01a7a8c6e9df54a19c521c68a535fbba01c65d
= <=>
2023-10-16 19:42:45 -0300

    Updates

--- static/BuGXhQcjmVxXtHMG70LkC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/418-6077c8a1d4cb047a.js","static/css/5ad4d55b98bee47f.css","static/chunks/pages/index-64f046f0c81e62bf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
