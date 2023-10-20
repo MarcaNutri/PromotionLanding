111f75831d4b44eae8c9f6e8cd7d4ccc147e3c2a
= <=>
2023-10-19 21:09:03 -0300

    update nutris

--- src/components/Partiner/index.jsx
+++ src/components/Partiner/index.jsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import Nutricionista from '../../../assets/parceiro.png'
 import Nutricionista1 from '../../../assets/rafaela.png'
 import Nutricionista2 from '../../../assets/natalia.png'
+import Nutricionista3 from '../../../assets/Miriam.png'
+import Nutricionista4 from '../../../assets/Bruna.png'
+import Nutricionista5 from '../../../assets/suzana.png'
 import Logo from '../../../assets/MarcaNutri.svg'
 import Check from '../../../assets/checkgreen.svg'
 import SmallCheck from '../../../assets/smallcheck.svg'
@@ -92,8 +95,8 @@ const Partiner = () => {
             <p className={`text-base font-semibold hidden md:block`}>
               R$: 190,00
             </p>
-            <p className={`text-base font-semibold hidden md:block`}>
-              Rua Andrade Neves 2998, Pelotas
+            <p className={`text-base font-semibold hidden md:block ${styles.text_container}`}>
+              Endereço: Rua Andrade Neves 2998, Pelotas
             </p>
 
             <div className="block md:hidden">
@@ -213,8 +216,8 @@ const Partiner = () => {
             <p className={`text-base font-semibold hidden md:block`}>
               R$: 120,00 - uma consulta
             </p>
-            <p className={`text-base font-semibold hidden md:block`}>
-              Gonçalves chaves 4721, Pelotas
+            <p className={`text-base font-semibold hidden md:block ${styles.text_container}`}>
+              Endereço: Gonçalves chaves 4721, Pelotas
             </p>
 
             <div className="block md:hidden">
@@ -282,34 +285,41 @@ const Partiner = () => {
           </div>
         </div>
 
-        {/* <div className={`${styles.hr_partiner} block md:hidden mb-8`} /> */}
+        <div className={`${styles.hr_partiner} block md:hidden mb-8`} />
         {/* Terceira Nutri */}
 
-        {/* <div className={`${styles.color_six} flex mb-8 md:mb-16`}>
+        <div className={`${styles.color_six} flex mb-8 md:mb-16`}>
           <div style={{ minWidth: '83px' }}>
             <Image
-              src={Nutricionista}
+              src={Nutricionista3}
               width={screenSize < 767 ? 83 : 198}
               alt="nutricionista"
             />
-            <p className={`text-sm font-semibold block md:hidden`}>CRN: 8321</p>
-            <p className={`text-smfont-semibold block md:hidden`}>R$ 190,00</p>
+            <p className={`text-sm font-semibold block md:hidden`}>CRN: 12.620D</p>
+            <p className={`text-smfont-semibold block md:hidden`}>R$ 250,00</p>
           </div>
 
           <div className="mr-3 ml-3">
             <p
               className={`${styles.color_fifhty} text-base md:text-2xl font-semibold`}
             >
-              Dra. Renata Silva
+              Mírian Lost
             </p>
             <p className={`text-xs md:text-base ${styles.text_container}`}>
-              Apaixonada pela nutrição e o que ela pode fazer e faz na vida das
-              pessoas. Ajudo pessoas a melhorarem sua qualidade de vida,
-              recuperarem a autoestima e alcançar o tão esperado objetivo, de
-              forma mais fácil, prazerosa e sem sofrimento
+            Trabalhei com nutrição clínica,
+principalmente com pacientes com Síndrome Metabólica (dislipidemia, hipertensão, diabetes tipo 2
+e obesidade) De alguns anos prá cá me dedico às questões da saúde da mulher, envelhecimento e saúde
+digestiva e intestinal, sempre com a nutrição integrativa como base.
             </p>
             <p className={`text-base font-semibold hidden md:block`}>
-              CRN: 8321
+              CRN: 12.620D
+            </p>
+            <p className={`text-base font-semibold hidden md:block`}>
+              R$: 250,00
+            </p>
+            <p className={`text-base font-semibold hidden md:block ${styles.text_container}`}>
+            Endereço: Rua General Teles
+671/302. - Pelotas
             </p>
 
             <div className="block md:hidden">
@@ -324,7 +334,7 @@ const Partiner = () => {
                     alt="Check - icon"
                     className="mr-1 md:mr-2"
                   />
-                  Esporte e Exercício Físico
+                  Nutrição clínica funcional
                 </p>
                 <p className={`flex items-center text-xs md:text-base mb-1`}>
                   <Image
@@ -333,7 +343,7 @@ const Partiner = () => {
                     alt="Check - icon"
                     className="mr-1 md:mr-2"
                   />
-                  Vegetarianismo e Veganismo
+                  Saúde da mulher
                 </p>
                 <p className={`flex items-center text-xs md:text-base mb-1`}>
                   <Image
@@ -342,12 +352,12 @@ const Partiner = () => {
                     alt="Check - icon"
                     className="mr-1 md:mr-2"
                   />
-                  Nutrição Funcional
+                  Saúde Digestiva e Intestinal.
                 </p>
               </div>
-              <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999254275", 'Dra. Renata Silva')}}>
-                <Image src={WhatsApp} width={33} alt="email" className="mr-2" />{' '}
-                Atendimento
+              <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53981112727", 'Mírian Lost')}}>
+                <Image src={WhatsApp} width={24} alt="email" className="mr-2" />{' '}
+                Agendar Consulta
               </button>
             </div>
           </div>
@@ -363,7 +373,7 @@ const Partiner = () => {
                 alt="Check - icon"
                 className="mr-1 md:mr-2"
               />
-              Esporte e Exercício Físico
+              Nutrição clínica funcional
             </p>
             <p className={`flex items-center text-xs md:text-base mb-2`}>
               <Image
@@ -372,7 +382,7 @@ const Partiner = () => {
                 alt="Check - icon"
                 className="mr-1 md:mr-2"
               />
-              Vegetarianismo e Veganismo
+              Saúde da mulher
             </p>
             <p className={`flex items-center text-xs md:text-base mb-2`}>
               <Image
@@ -381,48 +391,53 @@ const Partiner = () => {
                 alt="Check - icon"
                 className="mr-1 md:mr-2"
               />
-              Nutrição Funcional
+              Saúde Digestiva e Intestinal.
             </p>
-            <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999254275", 'Dra. Renata Silva')}}>
+            <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53981112727", 'Mírian Lost')}}>
               <Image
                 src={WhatsApp}
-                width={screenSize < 767 ? 24 : 33}
+                width={screenSize < 767 ? 24 : 26}
                 alt="email"
                 className="mr-1"
               />{' '}
-              Atendimento
+              Agendar Consulta
             </button>
           </div>
         </div>
 
-        <div className={`${styles.hr_partiner} block md:hidden mb-8`} /> */}
+        <div className={`${styles.hr_partiner} block md:hidden mb-8`} />
+
         {/* Quarta Nutri */}
 
-        {/* <div className={`${styles.color_six} flex mb-8 md:mb-16`}>
+        <div className={`${styles.color_six} flex mb-8 md:mb-16`}>
           <div style={{ minWidth: '83px' }}>
             <Image
-              src={Nutricionista}
+              src={Nutricionista4}
               width={screenSize < 767 ? 83 : 198}
               alt="nutricionista"
             />
-            <p className={`text-sm font-semibold block md:hidden`}>CRN: 8321</p>
-            <p className={`text-smfont-semibold block md:hidden`}>R$ 190,00</p>
+            <p className={`text-sm font-semibold block md:hidden`}>CRN: 17216</p>
+            <p className={`text-smfont-semibold block md:hidden`}>R$ 180,00</p>
           </div>
 
           <div className="mr-3 ml-3">
             <p
               className={`${styles.color_fifhty} text-base md:text-2xl font-semibold`}
             >
-              Dra. Renata Silva
+              Bruna Padilha
             </p>
             <p className={`text-xs md:text-base ${styles.text_container}`}>
-              Apaixonada pela nutrição e o que ela pode fazer e faz na vida das
-              pessoas. Ajudo pessoas a melhorarem sua qualidade de vida,
-              recuperarem a autoestima e alcançar o tão esperado objetivo, de
-              forma mais fácil, prazerosa e sem sofrimento
+            Nutricionista desde 2019, já acompanhei centenas de pessoas ao longo desse tempo. Meu formato de trabalho é inegociável: proporcionar resultados e autonomia alimentar sem abrir mão dos princípios básicos de saúde e sem corromper minha principal responsabilidade como profissional nutricionista que é resgatar a saúde e aumentar o tempo e qualidade de vida.
             </p>
             <p className={`text-base font-semibold hidden md:block`}>
-              CRN: 8321
+              CRN: 17216
+            </p>
+            <p className={`text-base font-semibold hidden md:block`}>
+              R$: 180,00
+            </p>
+            <p className={`text-base font-semibold hidden md:block ${styles.text_container}`}>
+            Endereço: Rua Major de Góes
+Monteiro Cícero, 164 - sala 302
             </p>
 
             <div className="block md:hidden">
@@ -458,9 +473,9 @@ const Partiner = () => {
                   Nutrição Funcional
                 </p>
               </div>
-              <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999254275", 'Dra. Renata Silva')}}>
-                <Image src={WhatsApp} width={33} alt="email" className="mr-2" />{' '}
-                Atendimento
+              <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53981101067", 'Bruna Padilha')}}>
+                <Image src={WhatsApp} width={24} alt="email" className="mr-2" />{' '}
+                Agendar Consulta
               </button>
             </div>
           </div>
@@ -496,19 +511,135 @@ const Partiner = () => {
               />
               Nutrição Funcional
             </p>
-            <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999254275", 'Dra. Renata Silva')}}>
+            <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53981101067", 'Bruna Padilha')}}>
               <Image
                 src={WhatsApp}
-                width={screenSize < 767 ? 24 : 33}
+                width={screenSize < 767 ? 24 : 26}
                 alt="email"
                 className="mr-1"
               />{' '}
-              Atendimento
+              Agendar Consulta
             </button>
           </div>
         </div>
 
-        <div className={`${styles.hr_partiner} block md:hidden mb-8`} /> */}
+        <div className={`${styles.hr_partiner} block md:hidden mb-8`} />
+
+        {/* Quinta Nutri */}
+
+        <div className={`${styles.color_six} flex mb-8 md:mb-16`}>
+          <div style={{ minWidth: '83px' }}>
+            <Image
+              src={Nutricionista5}
+              width={screenSize < 767 ? 83 : 198}
+              alt="nutricionista"
+            />
+            <p className={`text-sm font-semibold block md:hidden`}>CRN: 11487P</p>
+            <p className={`text-smfont-semibold block md:hidden`}>R$ 150,00</p>
+          </div>
+
+          <div className="mr-3 ml-3">
+            <p
+              className={`${styles.color_fifhty} text-base md:text-2xl font-semibold`}
+            >
+              Suzana
+            </p>
+            <p className={`text-xs md:text-base ${styles.text_container}`}>
+              Desde o início da minha formação estudo a nutrição voltada ao comportamento alimentar, história e cultura da alimentação. Atualmente trabalho a nutrição clínica com um olhar mais consciente e sustentável.
+            </p>
+            <p className={`text-base font-semibold hidden md:block`}>
+              CRN: 11487P
+            </p>
+            <p className={`text-base font-semibold hidden md:block`}>
+              R$: 150,00
+            </p>
+            <p className={`text-base font-semibold hidden md:block ${styles.text_container}`}>
+            Endereço: São José do Norte-RS, rua Miguel Roig, 253
+            </p>
+
+            <div className="block md:hidden">
+              <div className="flex flex-col justify-center">
+                <p className={`text-xs md:text-base font-semibold mb-2 mt-1`}>
+                  Especialidades
+                </p>
+                <p className={`flex items-center text-xs md:text-base mb-1`}>
+                  <Image
+                    src={screenSize < 767 ? SmallCheck : Check}
+                    width={screenSize < 767 ? 10.8 : 20}
+                    alt="Check - icon"
+                    className="mr-1 md:mr-2"
+                  />
+                  Comportamento Alimentar
+                </p>
+                <p className={`flex items-center text-xs md:text-base mb-1`}>
+                  <Image
+                    src={screenSize < 767 ? SmallCheck : Check}
+                    width={screenSize < 767 ? 10.8 : 20}
+                    alt="Check - icon"
+                    className="mr-1 md:mr-2"
+                  />
+                  Nutrição Clínica
+                </p>
+                <p className={`flex items-center text-xs md:text-base mb-1`}>
+                  <Image
+                    src={screenSize < 767 ? SmallCheck : Check}
+                    width={screenSize < 767 ? 10.8 : 20}
+                    alt="Check - icon"
+                    className="mr-1 md:mr-2"
+                  />
+                  Cultura da Alimentação
+                </p>
+              </div>
+              <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999903536", 'Suzana')}}>
+                <Image src={WhatsApp} width={24} alt="email" className="mr-2" />{' '}
+                Agendar Consulta
+              </button>
+            </div>
+          </div>
+
+          <div className="hidden md:block">
+            <p className={`text-xs md:text-base font-semibold mb-2`}>
+              Especialidades
+            </p>
+            <p className={`flex items-center text-xs md:text-base mb-2`}>
+              <Image
+                src={screenSize < 767 ? SmallCheck : Check}
+                width={screenSize < 767 ? 10.8 : 20}
+                alt="Check - icon"
+                className="mr-1 md:mr-2"
+              />
+              Comportamento Alimentar
+            </p>
+            <p className={`flex items-center text-xs md:text-base mb-2`}>
+              <Image
+                src={screenSize < 767 ? SmallCheck : Check}
+                width={screenSize < 767 ? 10.8 : 20}
+                alt="Check - icon"
+                className="mr-1 md:mr-2"
+              />
+              Nutrição Clínica
+            </p>
+            <p className={`flex items-center text-xs md:text-base mb-2`}>
+              <Image
+                src={screenSize < 767 ? SmallCheck : Check}
+                width={screenSize < 767 ? 10.8 : 20}
+                alt="Check - icon"
+                className="mr-1 md:mr-2"
+              />
+                Cultura da Alimentação
+            </p>
+            <button className={`flex ${styles.whats_button} mt-2 hover:brightness-90 ease-in-out duration-200`} onClick={() => {openWhatsApp("53999903536", 'Suzana')}}>
+              <Image
+                src={WhatsApp}
+                width={screenSize < 767 ? 24 : 26}
+                alt="email"
+                className="mr-1"
+              />{' '}
+              Agendar Consulta
+            </button>
+          </div>
+        </div>
+
       </div>
     </div>
   )
