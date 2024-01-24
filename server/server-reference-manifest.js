e890b53e51da9455effca28955ffb86d4f133c6d
= <=>
2024-01-24 16:37:38 -0300

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Tg8RWQd5LYvFXPM5/MIY/OQDoakUZxKLlHVt3qt54pc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"OB3Vp4RrH28p+X5PSHHwW9mLfzqaX0/0qdM6XMjnA1k=\"}"
