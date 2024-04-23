46de28b2deb10b82d6e4d5d640aa648728b4dc49
= <=>
2024-04-23 20:41:54 -0300

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"L0Fy10d+b6PNT0ZLiikkPA4RyhE79irXj5KoGB/+etc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"mSD9Xzvud8tZlIAwDzFei9n+EgGZq11rVzw0JhZbZ+U=\"}"
